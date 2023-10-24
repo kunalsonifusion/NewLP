acd470f2355f83062c6d4639f8003dac92205948
Kunal Soni <121236754+kunalsonifusion@users.noreply.github.com>
2023-10-24 11:33:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  <link rel="shortcut icon" href="https://www2023.fusionfirst.com/img/FBSPL/header/FBSPL_logo.png" type="image/x-icon" />
+
+</head>
+<body>
+    
+</body>
+</html> -->
+
+
+<?php
+$baseurl = ".";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="Discover FBSPL's comprehensive Insurance Business Outsourcing Services at AppliedNet2023. Elevate your insurance operations with our expert solutions and innovative strategies." />
+  <title>
+    FBSPL Insurance Business Outsourcing Services | AppliedNet2023
+  </title>
+  <link rel="shortcut icon" href="https://www2023.fusionfirst.com/img/FBSPL/header/FBSPL_logo.png" type="image/x-icon" />
+  <link rel="stylesheet" href="<?= $baseurl ?>/assets/css/owl.carousel.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+  <meta name="keywords" content="insurance agency customer service outsourcing, claims management outsourcing, insurance claims outsourcing, outsource insurance claims processing, Policy processing outsourcing services, claims Management Outsourcing Services, Insurance bpo, bpo in insurance " />
+  <!-- Jquey -->
+  <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
+
+  <!-- font -->
+  <script src="https://kit.fontawesome.com/f93e6dc96d.js" crossorigin="anonymous"></script>
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+  <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+  <link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+Light&display=swap" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="./assets/css/cs.css?d=<?= date("Y-m-d-H-i-s", time()) ?>" />
+  <link rel="stylesheet" href="./assets/css/new.css?d=<?= date("Y-m-d-H-i-s", time()) ?>" />
+</head>
+
+<body>
+</body>
+</html>
